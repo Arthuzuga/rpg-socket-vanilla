76140d0f126e11f3e9f7b8e547eaa984d9effe48
Joao Tavares <joao.arthur.tavares@gmail.com>
2022-02-11 22:43:50 -0300

    feat: add socket.io serve config

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
 
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/views'));
+
+io.on('connection', (socket) => {
+  socket.on('life', (msg) => {
+    console.log(msg)
+    io.emit('life', {
+      currentHP: msg,
+      fullHP: 100,
+    });
+  });
+  socket.on('mana', (msg) => {
+    console.log(msg)
+    io.emit('mana', {
+      currentMana: msg,
+      fullMana: 100,
+    });
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.get('/', function (req, res) {
   res.sendFile('/views/index.html')
@@ -21,6 +45,6 @@ app.get('/game', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/gameRoom.html'))
 })
 
-app.listen(5000)
-
-console.log(`IT'S ALIVE`);
+server.listen(5000, () => {
+  console.log(`IT'S ALIVE`);
+})
